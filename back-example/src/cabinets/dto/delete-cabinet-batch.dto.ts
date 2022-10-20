e9486177d7eeabbfba655c4bd0e11fdbea79db25
Radalls <leosallard@gmail.com>
2022-10-20 12:56:05 +0200

    removed backend

--- back-example/src/cabinets/dto/delete-cabinet-batch.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsArray, IsUUID } from 'class-validator';
-import { DeleteCabinetDto } from './delete-cabinet.dto';
-
-export class DeleteCabinetBatchDto extends DeleteCabinetDto {
-	@IsArray()
-	@IsUUID('all', { each: true })
-	ids: string[];
-}
--- back-example/src/cabinets/dto/index.ts
+++
@@ -1,7 +0,0 @@
-export { CreateCabinetDto } from './create-cabinet.dto';
-export { UpdateCabinetDto } from './update-cabinet.dto';
-export { DeleteCabinetBatchDto } from './delete-cabinet-batch.dto';
-export { EstablishmentDto } from './establishment.dto';
-export { UpdateCabinetBatchDto } from './update-cabinet-batch.dto';
-export { DeleteCabinetDto } from './delete-cabinet.dto';
-export { CreateCopyDto } from './create-copy.dto';
